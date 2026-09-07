e475213d44a52321a5a136610a8d5a8c61dfba44
sarithadevi9520 <sarithadevi.gudapati@sasi.ac.in>
2026-09-07 20:10:52 +0530

    Rename Day 1 to Day 01

--- Day 2/Functions/prime_numbers.py
+++ Day 2/Functions/prime_numbers.py
@@ -1,10 +1,11 @@
-def prime_num(num):
-    if num<2:
-       print(num)
-    for i in range(2,num):
-        if num%i==0:
-            break
-    print(i)
+def prime_num(a,b):
+    for i in range(a,b+1):
+       if i>1:
+          for num in range(2,i):
+            if i%num==0:
+               break
+          else:
+              print(i)
 
-print(prime_num(56))
+print(prime_num(0,100))
         
